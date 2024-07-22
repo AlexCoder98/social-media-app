4471692ea39f202e03f7807c21984038442053ef
Oleksandr Yurchuk <oleksandr.yurchuk@eltron.pl>
2024-07-22 15:35:38 +0200

    Testing connection to the backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 
@@ -7,6 +9,16 @@ import './styles/App.css';
 
 function App() {
   const posts = useAppSelector(state => state.posts.posts);
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('/users').then(res => {
+      return res.json();
+    }).then(data => {
+      console.log(data);
+    })
+  }, [])
 
   console.log('POSTS ARRAY');
   console.log(posts);
