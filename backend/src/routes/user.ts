1d4ab312b70013cf6dc440b570942f49b468e373
Oleksandr Yurchuk <syurchuk.98@gmail.com>
2024-09-20 22:33:59 +0200

    getProfile route has changed, now it is just /profile

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -9,7 +9,7 @@ import { getUser, getEditProfile, postEditProfile } from '../controllers/user';
 
 const router = express.Router();
 
-router.get('/profile/:userId', isAuthenticated, getUser);
+router.get('/profile', isAuthenticated, getUser);
 
 router.get('/profile/:userId/edit', isAuthenticated, getEditProfile);
 
